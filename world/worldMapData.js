84d8c98791fd3cd9a93cf3d560068ef531269e7e
tapwatero <charliewoolf20@gmail.com>
2022-04-09 22:47:12 +0100

    Quick Url Fix

--- world/worldMapData.js
+++ world/worldMapData.js
@@ -11,7 +11,7 @@ var simplemaps_worldmap_mapdata={
     state_description: "State description",
     state_color: "#88A4BC",
     state_hover_color: "#3B729F",
-    state_url: "http://simplemaps.com",
+    state_url: "",
     border_size: 0.75,
     all_states_inactive: "no",
     all_states_zoomable: "no",
